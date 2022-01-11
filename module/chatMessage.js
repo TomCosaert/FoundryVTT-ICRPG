c4e520fc7e7683e82f7f34e3254e874fe9b313aa
jessev14 <jessevo321@gmail.com>
2022-01-11 02:34:30 -0500

    improve d20 roll flagging

--- module/chatMessage.js
+++ module/chatMessage.js
@@ -1,17 +1,21 @@
 export class IcrpgChatMessage extends ChatMessage {
     async _preCreate(data, options, user) {
-        if (this.isRoll) {
+        if (this.roll.terms[0].faces === 20) {
             const globalDC = game.settings.get("icrpg", "globalDC");
             const pass = this.roll.total >= globalDC;
             this.data.update({ "flags.icrpg": { pass } });
         }
+
         await super._preCreate(data, options, user)
     }
 
-    async getHTML() {   
+    async getHTML() {
         const html = await super.getHTML();
-        if (!this.isRoll) return html;
         
+        const icrpgFlags = this.data.flags.icrpg || {};
+        const passInFlags = "pass" in icrpgFlags;
+        if (!passInFlags) return html;
+
         if (this.data.flags.icrpg.pass) html.find("h4.dice-total").addClass("icrpg-pass");
         else html.find("h4.dice-total").addClass("icrpg-fail");
 
