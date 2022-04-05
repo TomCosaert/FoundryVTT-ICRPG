51d8c6da4d74f7f4f904df9d683e9b7290de402e
jessev14 <jessevo321@gmail.com>
2022-04-05 15:43:17 -0400

    Revert "document dataModel updates"
    This reverts commit 0586ef9587c1b282b32b7bd1ef881a2564a3c6ab.

--- module/item/item.js
+++ module/item/item.js
@@ -10,9 +10,9 @@ export class IcrpgItem extends Item {
     super.prepareData();
 
     // Get the Item's data
-    const itemData = this;
-    const actorData = this.actor ? this.actor : {};
-    const data = itemData.system;
+    const itemData = this.data;
+    const actorData = this.actor ? this.actor.data : {};
+    const data = itemData.data;
   }
 
   async rollCheck() {
